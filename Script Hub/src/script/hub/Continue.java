1fd3dbf9a010699dedd5719419d9ddd221b933e0
Dylan <Dylan@Home>
2018-06-12 23:43:50 +0100

    Merge origin/master
    Conflicts:
    	.gitignore

--- Script Hub/src/script/hub/Continue.java
+++ Script Hub/src/script/hub/Continue.java
@@ -92,7 +92,8 @@ JFrame newFrame = new JFrame();
     }// </editor-fold>//GEN-END:initComponents
 
     private void jButton1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton1ActionPerformed
-        // TODO add your handling code here:
+     MainScreen myscreen = new MainScreen();
+     newFrame.dispose();
     }//GEN-LAST:event_jButton1ActionPerformed
 
 
